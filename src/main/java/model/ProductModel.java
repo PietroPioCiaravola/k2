2e1518064e5cb07f141cea1ea6dde603d36e729a
ciara <ciara@172.19.196.10>
2024-06-03 14:23:21 +0200

    id-1 resolved

--- src/main/java/model/ProductModel.java
+++ src/main/java/model/ProductModel.java
@@ -72,8 +72,6 @@ public class ProductModel {
 				bean.setImmagine(rs.getString("model"));
 			}
 			
-			
-
 		} finally {
 			try {
 				if (preparedStatement != null)
@@ -93,7 +91,7 @@ public class ProductModel {
 
 		int result = 0;
 
-		String deleteSQL = "UPDATE " + ProductModel.TABLE_NAME + "SET deleted = false WHERE codice = ?";
+		String deleteSQL = "UPDATE " + ProductModel.TABLE_NAME + " SET deleted = true WHERE codice = ?";
 
 		try {
 			connection = DriverManagerConnectionPool.getConnection();
@@ -113,75 +111,57 @@ public class ProductModel {
 		}
 		return (result != 0);
 	}
+	
 	public synchronized Collection<ProductBean> doRetrieveAll(String where) throws SQLException {
 		Connection connection = null;
-		Connection connection2 = null;
 		PreparedStatement preparedStatement = null;
-		PreparedStatement preparedStatement2 = null;
-
 		Collection<ProductBean> products = new LinkedList<ProductBean>();
 
-		String selectSQL = "SELECT * FROM " + ProductModel.TABLE_NAME + " WHERE deleted = 'false' AND nomeTipologia = '" + where + "'";
-		String sql2 = "SELECT AVG(votazione) FROM Recensione WHERE codiceProdotto = ?";
+		String selectSQL = "SELECT * FROM " + ProductModel.TABLE_NAME + " WHERE deleted = false AND nomeTipologia = ?";
 		
 		try {
-			connection = DriverManagerConnectionPool.getConnection();
-			preparedStatement = connection.prepareStatement(selectSQL);
-
-			ResultSet rs = preparedStatement.executeQuery();
-
-			while (rs.next()) {
-				ProductBean bean = new ProductBean();
-				
-				int codiceProdotto = rs.getInt("codice");
-				bean.setCodice(codiceProdotto);
-				bean.setNome(rs.getString("nome"));
-				bean.setDescrizione(rs.getString("descrizione"));
-				bean.setPrezzo(rs.getDouble("prezzo"));
-				bean.setSpedizione(rs.getDouble("speseSpedizione"));
-				bean.setEmail(rs.getString("emailVenditore"));
-				bean.setTag(rs.getString("tag"));
-				bean.setTipologia(rs.getString("nomeTipologia"));
-				bean.setData(rs.getDate("dataAnnuncio"));
-				bean.setImmagine(rs.getString("model"));
-				
-				connection2 = DriverManagerConnectionPool.getConnection();
-				preparedStatement2 = connection2.prepareStatement(sql2);
-				preparedStatement2.setInt(1, codiceProdotto);
-				ResultSet rs2 = preparedStatement2.executeQuery();
-				if (rs2.next()) {
-					bean.setVotazione(rs2.getDouble(1));
-				}
-				
-				products.add(bean);
-			}
-
+		    connection = DriverManagerConnectionPool.getConnection();
+		    preparedStatement = connection.prepareStatement(selectSQL);
+		    preparedStatement.setString(1, where);
+		 
+		    ResultSet rs = preparedStatement.executeQuery();
+		   
+		    while (rs.next()) {
+		        ProductBean bean = new ProductBean();
+		        bean.setCodice(rs.getInt("codice"));
+		        bean.setNome(rs.getString("nome"));
+		        bean.setDescrizione(rs.getString("descrizione"));
+		        bean.setPrezzo(rs.getDouble("prezzo"));
+		        bean.setSpedizione(rs.getDouble("speseSpedizione"));
+		        bean.setEmail(rs.getString("emailVenditore"));
+		        bean.setTag(rs.getString("tag"));
+		        bean.setTipologia(rs.getString("nomeTipologia"));
+		        bean.setData(rs.getDate("dataAnnuncio"));
+		        bean.setImmagine(rs.getString("model"));
+		        products.add(bean);
+		    }
 		} finally {
-			try {
-				if (preparedStatement != null)
-					preparedStatement.close();
-			} finally {
-				if (connection2 != null) {
-					DriverManagerConnectionPool.releaseConnection(connection2);
-				}
-				if (connection != null) {
-					DriverManagerConnectionPool.releaseConnection(connection);
-				}
-			}
+		    try {
+		        if (preparedStatement != null)
+		            preparedStatement.close();
+		    } finally {
+		        if (connection != null) {
+		            DriverManagerConnectionPool.releaseConnection(connection);
+		        }
+		    }
 		}
 		return products;
 	}
 	
 	public synchronized Collection<ProductBean> deleteProduct(int codiceProdotto, Collection<ProductBean> lista) {
-		String sql = "UPDATE Prodotto SET deleted = ? WHERE codice = ?";
+		String sql = "UPDATE Prodotto SET deleted = true WHERE codice = ?";
 		Connection con = null;
 		PreparedStatement ps = null;
 		
 		try {
 			con = DriverManagerConnectionPool.getConnection();
 			ps = con.prepareStatement(sql);
-			ps.setBoolean(1, true);
-			ps.setInt(2, codiceProdotto);
+			ps.setInt(1, codiceProdotto);
 			
 			ps.executeUpdate();
 			con.commit();
@@ -250,8 +230,8 @@ public class ProductModel {
 				bean.setVoto(rs.getInt(1));
 				bean.setTesto(rs.getString(2));
 			}
-				return bean;
-			}
+			return bean;
+		}
 		catch(Exception e) {
 			return null;
 		}
@@ -279,9 +259,9 @@ public class ProductModel {
 				nome = rs.getString(1);
 				tipologia = rs.getString(2);
 			}
-				String msg = "Dai un'occhiata a " + nome + " in " + tipologia;
-				return msg;
-			}
+			String msg = "Dai un'occhiata a " + nome + " in " + tipologia;
+			return msg;
+		}
 		catch(Exception e) {
 			return riprova;
 		}
@@ -292,3 +272,4 @@ public class ProductModel {
 		}
 	}
 }
+
